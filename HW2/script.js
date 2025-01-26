b576b8327ca2c5c3b2bda0771383f0d9866be1c5
Zrozen <hs144269@umconnect.umt.edu>
2025-01-26 16:42:00 -0700

    Homework 2

---
+++ HW2/script.js
@@ -0,0 +1,32 @@
+/*
+Author:Helen Sajna-Gross
+File Name: HW2
+
+Date: 1/27/2025
+Purpose: To list some favorite websites, games, and artists.
+Secondary purpose to test Muliti-line Comments
+*/
+
+//the console log/other begines below
+
+
+//websites
+console.log("My favorite websites:");
+console.log("1. 1.archiveofourown.org- Great for Fanfiction.");
+console.log("2. www1.flightrising.com - For dragon collecting.");
+console.log("3. www.tumblr.com - A social media with no algorithm. ");
+//games
+console.log("\nMy favorite games:");
+console.log("1. Final Fantasy 14 - online multiplayer, excellent story and music.");
+console.log("2. World of Warcraft - online multiplayer, more suited to exploration, minimal loading screens, meh story, good music.");
+console.log("3. Pokemon -  Low stress and fun.");
+//artists
+console.log("\nMy favorite artists and their cultural importance:");
+console.log("1. Hayao Miyazaki - amazing hand-drawn animation, relatable stories, international appeal")
+console.log("2. Anne McCaffrey - mostly nondystopian seamless blend of science fiction and what became the fantsy genre")
+console.log("3. Andrzej Sapkowski - author of the Witcher series")
+
+//prompt
+const userAnswer = prompt("What is your Favorite color?");
+console.log("\nYour favorite color is:", userAnswer);
+
